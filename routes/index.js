049800cf36c786c3780a9484cb10f0fc109db6a0
jocelynemo <jocelyn.e.mo@gmail.com>
2026-04-24 21:09:50 -0400

    initial stub

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+import auth from './auth.js';
+import profile from './profile.js';
+import buildings from './buildings.js';
+import reviews from './reviews.js';
+import comments from './comments.js';
+import favorites from './favorites.js';
+import admin from './admin.js';
+import survey from './survey.js';
+
+const constructorMethod = (app) => {
+  app.get('/', async (req, res) => {
+    return res.render('pages/landing');
+  });
+
+  app.use('/', auth);
+  app.use('/profile', profile);
+  app.use('/buildings', buildings);
+  app.use('/reviews', reviews);
+  app.use('/comments', comments);
+  app.use('/favorites', favorites);
+  app.use('/admin', admin);
+  app.use('/survey', survey);
+
+  app.use('*', async (req, res) => {
+    return res.status(404).render('pages/error');
+  });
+};
+
+export default constructorMethod;
